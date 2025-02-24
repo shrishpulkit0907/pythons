6054be067e807a7699d6dcd7445891cf99c3ecc9
shrishpulkit0907 <shrish.aminrup@gmail.com>
2025-02-24 16:52:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-import yfinance as yf 
-cacao = yf.Ticker("CC=F")
-data = cacao.history(period="1d") 
-last_close = data['Close'].iloc[-1] 
-print(last_close)
+from flask import Flask, jsonify
+import yfinance as yf
+
+app = Flask(__name__)
+
+@app.route('/get_cacao_price', methods=['GET'])
+def get_cacao_price():
+    try:
+        cacao = yf.Ticker("CC=F")
+        data = cacao.history(period="1d")
+        last_close = data['Close'].iloc[-1]
+        return jsonify({"last_close_price": last_close})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
